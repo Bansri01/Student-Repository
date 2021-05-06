2a334ce8315c0f0cc0535a6d7cebacc4b18dfc82
Bansri01 <82782098+Bansri01@users.noreply.github.com>
2021-05-05 21:00:45 -0400

    Add files via upload

--- Student_Repository_Bansrikumari_Patel.py
+++ Student_Repository_Bansrikumari_Patel.py
@@ -1,13 +1,14 @@
 """Author: Bansri Patel
    Created: 4/10/2021
+   Updated: 4/19/2021
 
    Purpose:
-   create a data repository of courses, students, and instructors
+   create a data repository of courses, majors, students, and instructors
 """
 
 import os
 from HW08_Bansrikumari_Patel import file_reader
-from typing import List, Iterator, Tuple, DefaultDict
+from typing import List, Iterator, Tuple, DefaultDict, Set, Dict
 from collections import defaultdict
 from prettytable import PrettyTable
 
@@ -16,7 +17,9 @@ class Student:
     """student class that has student CWID, Name, Completed courses and grades.
        It has methods to add course and grade and to return student information
     """
-    st_field_names: List[str] = ["CWID", "Name", "Completed Courses"]
+    st_field_names: List[str] = ["CWID", "Name", "Major", "Completed Courses",
+                                 "Remaining Required", "Remaining Electives",
+                                 "GPA"]
 
     def __init__(self, cwid: str, name: str, major: str) -> None:
         """init method to initialize the attributes"""
@@ -29,14 +32,33 @@ class Student:
         """method to add courses and grades"""
         self._courses[course] = grade
 
-    def st_data(self) -> Tuple[str, str, List[str]]:
+    def compute_gpa(self) -> float:
+        """method to compute gpa"""
+        grades: Dict[str, float] = {"A": 4.00, "A-": 3.75, "B+": 3.25,
+                                    "B": 3.00, "B-": 2.75, "C+": 2.25,
+                                    "C": 2.00, "C-": 0.00, "D+": 0.00,
+                                    "D": 0.00, "D-": 0.00, "F": 0.00}
+        try:
+            total: float = sum([grades[grade] for grade in
+                                self._courses.values()]) / len(self._courses.
+                                                               values())
+            return round(total, 2)
+        except ZeroDivisionError as e:
+            print(e)
+
+    def st_data(self) -> Tuple[str, str, str, List[str], List[str],
+                               List[str], float]:
         """returns student row"""
-        return [self._cwid, self._name, sorted(self._courses.keys())]
+        major, passed, rem_required, rem_electives = self._major.\
+            remaining_courses(self._courses)
+        return [self._cwid, self._name, major, sorted(passed),
+                sorted(rem_required), sorted(rem_electives),
+                self.compute_gpa()]
 
 
 class Instructor:
     """Instructor class has instructor CWID, name, department, courses and
-       numberof students who took that course. It has methods to increase
+       number of students who took that course. It has methods to increase
        number of students who took their course and to return instructor
        information
     """
@@ -61,6 +83,46 @@ class Instructor:
             yield [self._cwid, self._name, self._department, course, students]
 
 
+class Major:
+    """Major class has required and elective courses and computes reamining
+       required and remaining elective courses
+    """
+    major_field_names: List[str] = ["Major", "Required Courses", "Electives"]
+    passing_grades: Set[str] = {'A', 'A-', 'B+', 'B', 'B-', 'C+', 'C'}
+
+    def __init__(self, department: str) -> None:
+        """init method to initialize the attributes"""
+        self._department: str = department
+        self._required: Set[str] = set()
+        self._electives: Set[str] = set()
+
+    def add_course(self, course: str, type_c: str) -> None:
+        """add required and elective courses"""
+        if type_c == 'R':
+            self._required.add(course)
+        elif type_c == 'E':
+            self._electives.add(course)
+        else:
+            raise ValueError("Invalid course type")
+
+    def remaining_courses(self, completed: Dict) -> Tuple[str, str, str, str]:
+        """compute remaining required and elective courses"""
+        completed: Dict = {course for course, grade in completed.items() if
+                           grade in Major.passing_grades}
+
+        rem_required: Set = self._required - completed
+        rem_electives: Set = self._electives
+        if self._electives.intersection(completed):
+            rem_electives = set()
+
+        return self._department, completed, rem_required, rem_electives
+
+    def major_data(self) -> Tuple[str, List[str], List[str]]:
+        """return majors row"""
+        return (self._department, sorted(self._required),
+                sorted(self._electives))
+
+
 class Repository:
     """Repository class has all the information. It reads data from student,
        instructor and grade files and print pretty tables for students and
@@ -72,8 +134,10 @@ class Repository:
         self._dir_path: str = dir_path
         self._students_dict: Dict[str, Student] = dict()
         self._instructors_dict: Dict[str, Instructor] = dict()
+        self._majors_dict: Dict[str, Major] = dict()
 
         try:
+            self._get_majors(os.path.join(dir_path, "majors.txt"))
             self._get_students(os.path.join(dir_path, "students.txt"))
             self._get_instructors(os.path.join(dir_path, "instructors.txt"))
             self._get_grades(os.path.join(dir_path, "grades.txt"))
@@ -88,37 +152,55 @@ class Repository:
                 print(self.student_pt())
                 print("Instructor Data")
                 print(self.instructor_pt())
+                print("Majors Data")
+                print(self.majors_pt())
+
+    def _get_majors(self, path: str) -> None:
+        """read majors data from file using file_reader into the container"""
+        try:
+            majors_data: Iterator(Tuple[str]) = file_reader(path, 3, sep="\t",
+                                                            header=True)
+            for major, flag, course in majors_data:
+                if major not in self._majors_dict:
+                    self._majors_dict[major] = Major(major)
+                self._majors_dict[major].add_course(course, flag)
+        except ValueError as v:
+            print(v)
 
     def _get_students(self, path: str) -> None:
         """read students data from file into the container"""
         try:
             students_data: Iterator(Tuple[str]) = file_reader(path, 3,
-                                                              sep="\t",
-                                                              header=False)
+                                                              sep=";",
+                                                              header=True)
             for cwid, name, major in students_data:
-                self._students_dict[cwid] = Student(cwid, name, major)
+                if major not in self._majors_dict:
+                    print(f"Student {cwid} '{name}' has unknown major {major}")
+                else:
+                    self._students_dict[cwid] = Student(cwid, name, self.
+                                                        _majors_dict[major])
         except ValueError as v:
-            raise ValueError(v)
+            print(v)
 
     def _get_instructors(self, path: str) -> None:
         """read instructors data from file using file_reader into the container
         """
         try:
             instructors_data: Iterator(Tuple[str]) = file_reader(path, 3,
-                                                                 sep="\t",
-                                                                 header=False)
+                                                                 sep="|",
+                                                                 header=True)
             for cwid, name, department in instructors_data:
                 self._instructors_dict[cwid] = Instructor(cwid, name,
                                                           department)
         except ValueError as v:
-            raise ValueError(v)
+            print(v)
 
     def _get_grades(self, path: str) -> None:
         """read data of grades from grades file and update student and
            instructor data.
         """
-        grades: Iterator(Tuple[str]) = file_reader(path, 4, sep="\t",
-                                                   header=False)
+        grades: Iterator(Tuple[str]) = file_reader(path, 4, sep="|",
+                                                   header=True)
         for st_cwid, course, grade, inst_cwid in grades:
             if st_cwid in self._students_dict:
                 self._students_dict[st_cwid].add_course(course, grade)
@@ -129,7 +211,7 @@ class Repository:
             else:
                 print(f"grade for unkown instructor {inst_cwid}")
 
-    def student_pt(self) -> None:
+    def student_pt(self) -> PrettyTable:
         """print pretty table for student"""
         p_table: PrettyTable = PrettyTable(field_names=Student.st_field_names)
 
@@ -138,7 +220,7 @@ class Repository:
 
         return p_table
 
-    def instructor_pt(self) -> None:
+    def instructor_pt(self) -> PrettyTable:
         """print pretty table for instructor"""
         p_table: PrettyTable = PrettyTable(field_names=Instructor.
                                            inst_field_names)
@@ -146,6 +228,15 @@ class Repository:
         for instructor in self._instructors_dict.values():
             for row in instructor.inst_data():
                 p_table.add_row(row)
+
+        return p_table
+
+    def majors_pt(self) -> PrettyTable:
+        """print pretty table for major"""
+        p_table: PrettyTable = PrettyTable(field_names=Major.major_field_names)
+
+        for major in self._majors_dict.values():
+            p_table.add_row(major.major_data())
 
         return p_table
 
--- Student_Repository_Test_Bansrikumari_Patel.py
+++ Student_Repository_Test_Bansrikumari_Patel.py
@@ -1,14 +1,36 @@
 """Author: Bansri Patel
    Created: 4/10/2021
+   Updated: 4/19/2021
 
    Purpose:
-   To test the Student and instructor class from HW09
+   To test the Major, Student and instructor class
 """
 
 import unittest
 import os
 from typing import Iterator, Tuple, Dict, List
-from HW09_Bansrikumari_Patel import Student, Instructor, Repository
+from HW10_Bansrikumari_Patel import Student, Instructor, Repository
+
+
+class TestMajor(unittest.TestCase):
+    """Testing Major class"""
+    def test_major(self) -> None:
+        """test cases to test Major class"""
+        self.path: str = "/Users/bansripatel/Desktop/ssw-810"
+        self.rt: Repository = Repository(self.path, False)
+        table3: List[Tuple(str, List[str], List[str])] = [majors.major_data()
+                                                          for majors in self.
+                                                          rt._majors_dict.
+                                                          values()
+                                                          ]
+        m_table: List[Tuple(str, List[str],
+                      List[str])] = [('SFEN', ['SSW 540', 'SSW 555', 'SSW 564',
+                                      'SSW 567'], ['CS 501', 'CS 513',
+                                      'CS 545']), ('SYEN', ['SYS 612',
+                                                   'SYS 671', 'SYS 800'],
+                                                   ['SSW 540', 'SSW 565',
+                                                    'SSW 810'])]
+        self.assertEqual(m_table, table3)
 
 
 class TestStudent(unittest.TestCase):
@@ -17,45 +39,78 @@ class TestStudent(unittest.TestCase):
         """test cases to test Student class"""
         self.path: str = "/Users/bansripatel/Desktop/ssw-810"
         self.rt: Repository = Repository(self.path, False)
-        table1: Dict[str: [str, str, List[str]]] = {cwid: student.st_data()
-                                                    for cwid, student in
-                                                    self.rt._students_dict.
-                                                    items()
-                                                    }
-        st_table: Dict[str: [str, str, List[str]]] = {'10103':
-                                                      ['10103', 'Baldwin, C',
-                                                       ['CS 501', 'SSW 564',
-                                                        'SSW 567', 'SSW 687']],
-                                                      '10115':
-                                                      ['10115', 'Wyatt, X',
-                                                       ['CS 545', 'SSW 564',
-                                                        'SSW 567', 'SSW 687']],
-                                                      '10172':
-                                                      ['10172', 'Forbes, I',
-                                                       ['SSW 555', 'SSW 567']],
-                                                      '10175':
-                                                      ['10175', 'Erickson, D',
-                                                       ['SSW 564', 'SSW 567',
-                                                        'SSW 687']],
-                                                      '10183':
-                                                      ['10183', 'Chapman, O',
-                                                       ['SSW 689']],
-                                                      '11399':
-                                                      ['11399', 'Cordova, I',
-                                                       ['SSW 540']],
-                                                      '11461':
-                                                      ['11461', 'Wright, U',
-                                                       ['SYS 611', 'SYS 750',
-                                                        'SYS 800']],
-                                                      '11658':
-                                                      ['11658', 'Kelly, P',
-                                                       ['SSW 540']],
-                                                      '11714':
-                                                      ['11714', 'Morton, A',
-                                                       ['SYS 611', 'SYS 645']],
-                                                      '11788':
-                                                      ['11788', 'Fuller, E',
-                                                       ['SSW 540']]}
+        table1: List[List[str, str, str, List[str],
+                     List[str], List[str], float]] = [student.st_data() for
+                                                      cwid, student in self.rt.
+                                                      _students_dict.items()]
+        st_table: List[List[str, str, str, List[str],
+                       List[str], List[str], float]] = [['10103', 'Baldwin, C',
+                                                         'SFEN', ['CS 501',
+                                                                  'SSW 564',
+                                                                  'SSW 567',
+                                                                  'SSW 687'], [
+                                                                  'SSW 540',
+                                                                  'SSW 555'],
+                                                         [], 3.44],
+                                                        ['10115', 'Wyatt, X',
+                                                        'SFEN', ['CS 545',
+                                                         'SSW 564', 'SSW 567',
+                                                                 'SSW 687'],
+                                                         ['SSW 540',
+                                                          'SSW 555'], [], 3.81
+                                                         ],
+                                                        ['10172', 'Forbes, I',
+                                                        'SFEN', ['SSW 555',
+                                                         'SSW 567'],
+                                                         ['SSW 540',
+                                                         'SSW 564'], ['CS 501',
+                                                         'CS 513', 'CS 545'
+                                                                      ], 3.88],
+                                                        ['10175',
+                                                        'Erickson, D', 'SFEN',
+                                                         ['SSW 564', 'SSW 567',
+                                                          'SSW 687'],
+                                                         ['SSW 540',
+                                                          'SSW 555'],
+                                                         ['CS 501', 'CS 513',
+                                                          'CS 545'], 3.58],
+                                                        ['10183', 'Chapman, O',
+                                                         'SFEN', ['SSW 689'],
+                                                         ['SSW 540', 'SSW 555',
+                                                          'SSW 564',
+                                                          'SSW 567'],
+                                                         ['CS 501', 'CS 513',
+                                                          'CS 545'], 4.0],
+                                                        ['11399', 'Cordova, I',
+                                                         'SYEN', ['SSW 540'],
+                                                         ['SYS 612', 'SYS 671',
+                                                          'SYS 800'], [], 3.0],
+                                                        ['11461', 'Wright, U',
+                                                         'SYEN', ['SYS 611',
+                                                                  'SYS 750',
+                                                                  'SYS 800'],
+                                                         ['SYS 612',
+                                                          'SYS 671'],
+                                                         ['SSW 540', 'SSW 565',
+                                                          'SSW 810'], 3.92],
+                                                        ['11658', 'Kelly, P',
+                                                         'SYEN', [],
+                                                         ['SYS 612', 'SYS 671',
+                                                          'SYS 800'],
+                                                         ['SSW 540',
+                                                          'SSW 565',
+                                                          'SSW 810'], 0.0],
+                                                        ['11714', 'Morton, A',
+                                                         'SYEN', ['SYS 611',
+                                                                  'SYS 645'],
+                                                         ['SYS 612', 'SYS 671',
+                                                          'SYS 800'],
+                                                         ['SSW 540', 'SSW 565',
+                                                          'SSW 810'], 3.0],
+                                                        ['11788', 'Fuller, E',
+                                                         'SYEN', ['SSW 540'],
+                                                         ['SYS 612', 'SYS 671',
+                                                          'SYS 800'], [], 4.0]]
 
         self.assertEqual(st_table, table1)
 
